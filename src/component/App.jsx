12716305db851ffbb7e95fea33b83493b2e71cb3
Johnny <po_bo@hotmail.com>
2018-01-07 18:38:59 -0800

    First commit

---
+++ src/component/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
